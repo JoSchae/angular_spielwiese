dda504e674d13830aeb5de100add71361ec5315b
Johannes <johannes_schaefer@gmx.net>
2019-11-01 17:41:23 +0100

    added simple authentication.service tests

--- src/app/_services/authentication/authentication.service.spec.ts
+++ src/app/_services/authentication/authentication.service.spec.ts
@@ -1,12 +1,59 @@
-import { TestBed } from '@angular/core/testing';
-
+import { TestBed, getTestBed } from '@angular/core/testing';
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
 import { AuthenticationService } from './authentication.service';
+import { environment } from 'src/environments/environment';
+import { IAuthentication } from 'src/app/_models/authentication.interface';
+import { HttpErrorResponse } from '@angular/common/http';
 
 describe('AuthenticationService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
-    const service: AuthenticationService = TestBed.get(AuthenticationService);
-    expect(service).toBeTruthy();
-  });
+    let injector: TestBed;
+    let service: AuthenticationService;
+    let httpMock: HttpTestingController;
+
+    const testToken = 'some_test_token';
+    const validResponse = ({token: testToken, isLoggedIn: undefined}) as IAuthentication;
+    const errorResponse = new ErrorEvent('test');
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [AuthenticationService]
+        });
+        injector = getTestBed();
+        service = injector.get(AuthenticationService);
+        httpMock = injector.get(HttpTestingController);
+    });
+
+    afterEach(() => {
+        httpMock.verify();
+    });
+
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
+    it('should get a token', () => {
+        service.getToken().subscribe(
+            response => {
+                console.log(response);
+                expect(response.authentication).toEqual(validResponse);
+            }
+        );
+        const authenticationRequest = httpMock.expectOne(
+            `${environment.apiUrl}/${environment.authenticationEndpoint}`
+        );
+        authenticationRequest.flush(validResponse);
+    });
+
+    it('should get error out', () => {
+        service.getToken().subscribe(
+            next => {},
+            err => expect(err.error).toEqual(errorResponse)
+        );
+        const authenticationRequest = httpMock.expectOne(
+            `${environment.apiUrl}/${environment.authenticationEndpoint}`
+        );
+        authenticationRequest.error(errorResponse);
+    });
 });
